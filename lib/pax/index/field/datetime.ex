82bde171cb0c49179bece174faa1cbce083fdad4
Nathan Shafer <nate@lotech.org>
2023-08-09 11:22:28 -0700

    Allow wrapping only between date and time

--- lib/pax/index/field/datetime.ex
+++ lib/pax/index/field/datetime.ex
@@ -4,14 +4,27 @@ defmodule Pax.Index.Field.Datetime do
   @impl Pax.Index.Field
   def init(_mod, opts) do
     %{
-      format: Keyword.get(opts, :format, "%a, %B %d, %Y %I:%M %p %Z")
+      format: Keyword.get(opts, :format, nil),
+      date_format: Keyword.get(opts, :format, "%a, %B %d, %Y"),
+      time_format: Keyword.get(opts, :format, "%I:%M %p")
     }
   end
 
   @impl Pax.Index.Field
   def render(_opts, nil), do: nil
 
-  def render(%{format: format}, value) do
-    Calendar.strftime(value, format)
+  def render(%{format: format}, value) when not is_nil(format) do
+    Phoenix.HTML.Tag.content_tag(:span, Calendar.strftime(value, format), style: "white-space: nowrap;")
+  end
+
+  def render(%{date_format: date_format, time_format: time_format}, value) do
+    date = Calendar.strftime(value, date_format)
+    time = Calendar.strftime(value, time_format)
+
+    [
+      Phoenix.HTML.Tag.content_tag(:span, date, style: "white-space: nowrap;"),
+      " ",
+      Phoenix.HTML.Tag.content_tag(:span, time, style: "white-space: nowrap;")
+    ]
   end
 end
