9aa35e1d155beaaf333aa5772453a5f06ff525c1
Nathan Shafer <nate@lotech.org>
2025-01-26 10:31:32 -0700

    Move watch mix task out of lib

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule Pax.MixProject do
   end
 
   # Specifies which paths to compile per environment.
+  defp elixirc_paths(:dev), do: ["lib", "dev"]
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
