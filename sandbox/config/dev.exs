45f4ca8a4cfe604f1c7f91ea997a5bae273a0d0d
Nathan Shafer <nate@lotech.org>
2025-04-17 15:24:31 -0700

    Setup db

--- sandbox/config/dev.exs
+++ sandbox/config/dev.exs
@@ -5,7 +5,7 @@ config :sandbox, Sandbox.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "sandbox_dev",
+  database: "pax_sandbox_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
