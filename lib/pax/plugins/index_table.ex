4dc173f81b8abe6c0724ada4fef830b881c258ee
Nathan Shafer <nate@lotech.org>
2025-10-03 16:54:39 -0700

    Default icon sizes in case css isn't loaded

--- lib/pax/plugins/index_table.ex
+++ lib/pax/plugins/index_table.ex
@@ -292,7 +292,13 @@ defmodule Pax.Plugins.IndexTable do
   # https://fontawesome.com/icons/caret-up?f=classic&s=solid
   defp sort_asc_icon(assigns) do
     ~H"""
-    <svg class="pax-icon pax-index-table-sort-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+    <svg
+      class="pax-icon pax-index-table-sort-icon"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 320 512"
+      height="1em"
+      width="1em"
+    >
       <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
       <path d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l256 0c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z" />
     </svg>
@@ -302,7 +308,13 @@ defmodule Pax.Plugins.IndexTable do
   # https://fontawesome.com/icons/caret-down?f=classic&s=solid
   defp sort_desc_icon(assigns) do
     ~H"""
-    <svg class="pax-icon pax-index-table-sort-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+    <svg
+      class="pax-icon pax-index-table-sort-icon"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 320 512"
+      height="1em"
+      width="1em"
+    >
       <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
       <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
     </svg>
@@ -312,7 +324,13 @@ defmodule Pax.Plugins.IndexTable do
   # https://fontawesome.com/icons/sort?f=classic&s=solid
   defp sort_icon(assigns) do
     ~H"""
-    <svg class="pax-icon pax-index-table-sort-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+    <svg
+      class="pax-icon pax-index-table-sort-icon"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 320 512"
+      height="1em"
+      width="1em"
+    >
       <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
       <path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8L32 224c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8l256 0c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z" />
     </svg>
--- lib/pax/plugins/pagination.ex
+++ lib/pax/plugins/pagination.ex
@@ -214,7 +214,7 @@ defmodule Pax.Plugins.Pagination do
 
   defp angles_left(assigns) do
     ~H"""
-    <svg class="pax-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    <svg class="pax-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="1em" width="1em">
       <!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->
       <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160zm352-160l-160 160c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L301.3 256 438.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0z" />
     </svg>
@@ -223,7 +223,7 @@ defmodule Pax.Plugins.Pagination do
 
   defp angles_right(assigns) do
     ~H"""
-    <svg class="pax-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    <svg class="pax-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="1em" width="1em">
       <!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->
       <path d="M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z" />
     </svg>
@@ -232,7 +232,7 @@ defmodule Pax.Plugins.Pagination do
 
   defp angle_left(assigns) do
     ~H"""
-    <svg class="pax-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+    <svg class="pax-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" height="1em" width="1em">
       <!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->
       <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
     </svg>
@@ -241,7 +241,7 @@ defmodule Pax.Plugins.Pagination do
 
   defp angle_right(assigns) do
     ~H"""
-    <svg class="pax-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+    <svg class="pax-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" height="1em" width="1em">
       <!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->
       <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
     </svg>
@@ -250,7 +250,7 @@ defmodule Pax.Plugins.Pagination do
 
   # defp ellipsis(assigns) do
   #   ~H"""
-  #   <svg class="pax-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+  #   <svg class="pax-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="1em" width="1em">
   #     <!--! Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. -->
   #     <path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z" />
   #   </svg>
